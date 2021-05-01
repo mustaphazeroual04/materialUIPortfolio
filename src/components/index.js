063c77ca7956fd91c89dbc0aa75bfa4de0f6c0c9
mustaphazeroual04 <mustaphazeroual01@gmail.com>
2021-05-01 17:10:45 +0000

    add header component

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,65 @@
 import React from 'react'
 import NavBar from './NavBar';
+import Header from './header';
+import Particles from 'react-particles-js';
+import {makeStyles} from '@material-ui/styles';
+
+const usestyles = makeStyles((theme) => ({
+    particlesCanva : {
+        position: "absolute"
+    }
+}));
+
 const Home = () => {
+    const classes = usestyles();
     return (
-        <div>
+        <>
+           
             <NavBar />
-        </div>
+            <Header />
+            <Particles 
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles:{
+                        number : {
+                            value : 45,
+                            density : {
+                                enable : true,
+                                value_area : 900
+                            }
+                        },
+                        shape : {
+                            type: "circle",
+                            stroke: {
+                                width:1,
+                                color : "tomato"
+                            }
+                        },
+                        size: {
+                            value : 10,
+                            random : true,
+                            anim: {
+                                enable : true,
+                                speed : 10,
+                                size_min : 0.1,
+                                sync: true
+                            }
+                        },
+                        opacity : {
+                            value: 1,
+                            random : true,
+                            animation : {
+                                enable : true,
+                                speed : 1,
+                                opacity_min : 0.1,
+                                sync : true
+                            }
+                        }
+                        
+                    }
+                }}
+            />
+        </>
     )
 }
 
